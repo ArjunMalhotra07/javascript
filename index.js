26a34fcdacf454fcbec8ae202066118192f03272
Arjun Malhotra <arjun03.malhotra@gmail.com>
2023-03-21 11:59:34 +0530

    Added 2 files

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = 'Arjun';
+console.log(name); 
+let interestRate = 0.3;
+console.log(interestRate);
+
+interestRate = 5.7;
+
+console.log(interestRate);
+const myAge = 20;
+
+console.log(myAge);
+let selectedColor = null;
