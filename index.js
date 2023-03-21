1912dc05d147ec4fd6feb5527f329eeae25f09e5
Arjun Malhotra <arjun03.malhotra@gmail.com>
2023-03-21 12:14:22 +0530

    Objects in JS

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ const myAge = 20;
 console.log(myAge);
 let selectedColor = null;
 console.log(selectedColor);
+
+let person = {
+    name : "Arjun Malhotra",
+    age : 90
+};
+console.log(person);
+
+person.name = "Arjun M.";
+person['age'] = 20;
+
+console.log(person.name);
+console.log(person['age']);
