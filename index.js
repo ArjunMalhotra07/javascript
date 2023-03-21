bd760148b42dd1c1d9661a1b65edc9aa04c47afa
Arjun Malhotra <arjun03.malhotra@gmail.com>
2023-03-21 14:27:15 +0530

    added Arrays code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const myAge = 20;
 console.log(myAge);
 let selectedColor = null;
 console.log(selectedColor);
-
+//! Objects in JS
 let person = {
     name : "Arjun Malhotra",
     age : 90
@@ -23,3 +23,12 @@ person['age'] = 20;
 
 console.log(person.name);
 console.log(person['age']);
+//! Arrays in JS
+let testArray =['colorA', 15, true];
+console.log(testArray[0]);
+console.log(testArray[1]);
+console.log(testArray[2]);
+console.log(testArray[3]);
+testArray[4] = "4th Object";
+testArray[6] = "6th Object";
+console.log(testArray);
