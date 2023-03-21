0a84f439e175e3e1cd088f63b6b515bfa0a61bc8
Arjun Malhotra <arjun03.malhotra@gmail.com>
2023-03-21 15:08:35 +0530

    Added functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name, lastName){
+    console.log("This is func greet "+name + ' '+ lastName);
+}
+
+greet('Arjun');
+
+
+function calculateSquare(number){
+    return number*number;
+}
+
+let sq = calculateSquare(55);
+console.log(sq);
